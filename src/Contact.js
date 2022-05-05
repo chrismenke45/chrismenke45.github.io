72becb265865806736c46a46e2d84c95f444f8a9
chrismenke45 <chrismenke45@gmail.com>
2022-05-04 17:46:07 -0700

    mobile friendly styling

--- src/Contact.js
+++ src/Contact.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Contact() {
     return (
-        <main id="contact">
+        <address id="contact">
             <h1>Chris Menke</h1>
             <h3>chrismenke45@gmail.com</h3>
             <h3><a href="https://github.com/chrismenke45" target="_blank" rel="noreferrer">github.com/chrismenke45</a></h3>
             <h3>La Crescenta, CA, USA</h3>
-        </main>
+        </address>
     );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,8 @@ function Home() {
     return (
         <div id="home">
             <h1>Hi, I'm Chris!</h1>
-            <img id='photo' src='./IMG_4657.jpg' alt='A Photo of Me Looking Profesional'></img>
-            <p>I'm a web developer from North Los Angeles. I have experience with building websites with HTML, CSS, JavaScript and React, but I’m looking to gain more professional experience as a web developer. Please checkout my projects on this website and reach out to me if you think I’d be the right fit for you!</p>
-            <br></br>
-
-            
+            <img id='photo' src='./IMG_4657.jpg' alt='Me'></img>
+            <p>I'm a web developer from North Los Angeles. I have experience with building websites with HTML, CSS, JavaScript, React, Node.js and MongoDB and I’m looking to gain more professional experience. Please checkout my projects and reach out to me if you think I’d be the right fit for you!</p>
         </div>
     );
 }
