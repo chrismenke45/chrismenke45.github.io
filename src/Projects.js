7bd8a652a5a79a54bf5b067e1fd7d6c3563de3ff
chrismenke45 <chrismenke45@gmail.com>
2022-06-30 17:32:16 -0700

    added twitter clone

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,13 @@
 import React from "react";
 import IndividualProject from "./IndividualProject";
+import twitterClone from "./projects/twitterClone";
 import wheresWaldo from "./projects/wheresWaldo";
 import shoppingCart from "./projects/shoppingCart";
-import memory from "./projects/memory";
+//import memory from "./projects/memory";
 import pikPak from "./projects/pikPak";
 
 function Projects() {
-    const projects = [pikPak, wheresWaldo, shoppingCart, memory]
+    const projects = [twitterClone, pikPak, wheresWaldo, shoppingCart]
     return (
         <main id="projects">
             {projects.map(project => {
