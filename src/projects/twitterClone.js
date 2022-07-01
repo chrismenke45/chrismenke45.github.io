99e59414a27b6568018a47bdf215d607291824b9
chrismenke45 <chrismenke45@gmail.com>
2022-06-30 17:44:14 -0700

    added github page of twitter client

--- src/projects/twitterClone.js
+++ src/projects/twitterClone.js
@@ -3,7 +3,7 @@ const twitterClone = {
     description: "My Twitter Clone was made with the MERN Stack and is composed of 2 sites: a frontend hosted on Github Pages and an api hosted on Heroku.  It allows users to sign in with their Twitter credentials (via Passport.js and Twitter OAuth).  This imports and prepopulates their user profile with their username and profile picture.  The user can then post on the clone with text or pictures, follow other users, and explore tweets and profiles.  None of the clone generated posts are shared to Twitter.",
     experience: 'This app had a lot go into it.  It allowed me to practice developing and testing a seperate back end and front end with the goal of bringin them together.  It also gave me the chance to write some responsive CSS to make it mobile friendly',
     siteUrl: "https://chrismenke45.github.io/twitter-client",
-    githubUrl: "https://github.com/chrismenke45/wheresWaldo",
+    githubUrl: "https://github.com/chrismenke45/twitter-client",
 
 }
 export default twitterClone
