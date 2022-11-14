6c2b4affa3249219cbd61227d4ab354b1bd33871
chrismenke45 <chrismenke45@gmail.com>
2022-11-14 14:28:28 -0800

    edited personal summary

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <div id="home">
             <h1>Hi, I'm Chris!</h1>
             <img id='photo' src='./IMG_4657.jpg' alt='Me'></img>
-            <p>I'm a web developer from North Los Angeles. I have experience with building websites with HTML, CSS, JavaScript, React, Node.js and MongoDB and I’m looking to gain more professional experience. Please checkout my projects and reach out to me if you think I’d be the right fit for you!</p>
+            <p>I'm a web developer with experience building websites with HTML, CSS, JavaScript, React, Node.js and MongoDB. I have also started to learn ruby. I’m looking to gain more professional experience. Please checkout my projects and reach out to me if you think I’d be the right fit for you!</p>
         </div>
     );
 }
