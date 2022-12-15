8dbb7dd71882f9035c18456c2645fd471ef23ea8
chrismenke45 <chrismenke45@gmail.com>
2022-12-15 09:53:00 -0800

    added snake game information

--- src/Projects.js
+++ src/Projects.js
@@ -1,14 +1,15 @@
 import React from "react";
 import IndividualProject from "./IndividualProject";
 import twitterClone from "./projects/twitterClone";
-import wheresWaldo from "./projects/wheresWaldo";
-import shoppingCart from "./projects/shoppingCart";
+//import wheresWaldo from "./projects/wheresWaldo";
+//import shoppingCart from "./projects/shoppingCart";
 import chess from "./projects/chess";
+import snake from "./projects/snake";
 //import memory from "./projects/memory";
 //import pikPak from "./projects/pikPak";
 
 function Projects() {
-    const projects = [twitterClone, chess, wheresWaldo, shoppingCart]
+    const projects = [twitterClone, chess, snake]
     return (
         <main id="projects">
             <div className="projectsTitleContainer">
---
+++ src/projects/snake.js
@@ -0,0 +1,9 @@
+const snake = {
+    name: "Snake Game",
+    description: "This is project is a full stack site using React.js for the front end and Ruby on Rails for an api to store high scores.",
+    experience: 'I made this because I wanted practice building a simple rails API and wanted to write some JavaScript/React after spending the past two months focusing on Ruby and Ruby on Rails. It allowed me to reorient myself with JavaScript while applying some bet practices I learned while studying Ruby',
+    siteUrl: "https://chrismenke45.github.io/snake_client/",
+    githubUrl: "https://github.com/chrismenke45/snake_client",
+
+}
+export default snake
