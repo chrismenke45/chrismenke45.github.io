bed60dc9b3c46ca80d96f6d16121062ba0d65fae
chrismenke45 <chrismenke45@gmail.com>
2022-12-15 10:13:26 -0800

    changed 'Heroku' to 'render.com'

--- src/projects/twitterClone.js
+++ src/projects/twitterClone.js
@@ -1,6 +1,6 @@
 const twitterClone = {
     name: "Twitter Clone",
-    description: "My Twitter Clone was made with the MERN Stack and is composed of 2 sites: a frontend hosted on Github Pages and an api hosted on Heroku.  It allows users to sign in with their Twitter credentials via Passport.js and Twitter OAuth.  This imports and prepopulates their user profile with their username and profile picture.  The user can then post on the clone with text or pictures, follow other users, and explore tweets and profiles.  All Clone generated posts are kept internal to the Twitter Clone.",
+    description: "My Twitter Clone was made with the MERN Stack and is composed of 2 sites: a frontend hosted on Github Pages and an api hosted on Render.com.  It allows users to sign in with their Twitter credentials via Passport.js and Twitter OAuth.  This imports and prepopulates their user profile with their username and profile picture.  The user can then post on the clone with text or pictures, follow other users, and explore tweets and profiles.  All Clone generated posts are kept internal to the Twitter Clone.",
     experience: 'This app had a lot go into it.  It allowed me to practice developing and testing a separate back end and front end with the goal of bringing them together.  It also gave me the chance to write some responsive CSS to make it mobile friendly',
     siteUrl: "https://chrismenke45.github.io/twitter-client",
     pingUrl: "https://twitter-api-9m7g.onrender.com",
