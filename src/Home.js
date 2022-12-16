9ac4ce3c1cc0a713c1a6d8d1c1f0772f70f93efb
chrismenke45 <chrismenke45@gmail.com>
2022-12-16 11:07:57 -0800

    added fadeIn on scroll, paused api pinging

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div id="home">
             <h1>Hi, I'm Chris!</h1>
-            <img id='photo' src='./IMG_4657.jpg' alt='Me'></img>
+            <img id='photo' className="fadeIn" src='./IMG_4657.jpg' alt='Me'></img>
             <p>I'm a web developer with experience building websites with HTML, CSS, JavaScript, React, Node.js and MongoDB. I have also started to learn ruby. I’m looking to gain more professional experience. Please checkout my projects and reach out to me if you think I’d be the right fit for you!</p>
         </div>
     );
--- src/IndividualProject.js
+++ src/IndividualProject.js
@@ -1,26 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 
 function IndividualProject(props) {
     const { project } = props
+    
+    const fadeInRef = useRef(null)
+
+    useLayoutEffect(() => {
+        window.addEventListener('scroll', scrollFadeIn)
+        return () => window.removeEventListener('scroll', scrollFadeIn)
+      }, [])
+  
+      const scrollFadeIn = () => {
+        const topPosition = fadeInRef.current.getBoundingClientRect().top
+          const scrollPosition = window.scrollY + window.innerHeight * 1.4
+          if (topPosition < scrollPosition) {
+            console.log(topPosition, scrollPosition, window.scrollY + window.innerHeight)
+            fadeInRef.current.classList.add("fadeIn")
+            fadeInRef.current.classList.remove("hiddenForFadeIn")
+          }
+      }
 
 
-    useEffect(() => {
-        if (project.pingUrl) {
-            let apiUrl = project.pingUrl
-            const options = {
-                method: 'GET',
-            };
-            return fetch(apiUrl, options)
-                .then((response) => {
-                    console.log(`${project.name} pinged`);
-                })
-                .catch(error => {
-                    console.error('Error:', error)
-                })
-        }
-    })
+    // useEffect(() => {
+    //     if (project.pingUrl) {
+    //         let apiUrl = project.pingUrl
+    //         const options = {
+    //             method: 'GET',
+    //         };
+    //         return fetch(apiUrl, options)
+    //             .then((response) => {
+    //                 console.log(`${project.name} pinged`);
+    //             })
+    //             .catch(error => {
+    //                 console.error('Error:', error)
+    //             })
+    //     }
+    // })
     return (
-        <div className="individualProject">
+        <div ref={fadeInRef} className="hiddenForFadeIn individualProject">
             <h3 className="projectName">{project.name}</h3>
             <p>{project.description}</p>
             <details>
