9fb03cc251b7e05b35a4824239b0826933cfd574
chrismenke45 <chrismenke45@gmail.com>
2022-12-16 12:39:58 -0800

    added information about testing

--- src/projects/chess.js
+++ src/projects/chess.js
@@ -1,7 +1,7 @@
 const chess = {
     name: "Chess",
     description: "This is project is command-line chess written in ruby.  It can either be played with 2 players or against a computer. The game will end and a winner will be declared when a player is checkmated. ",
-    experience: 'This project gave me an opportunity to practice my Object Oriented Programming skills by making classes and modules to support each distinct piece without rewriting any code. I also got to write algorithms to detect if there was a check or checkmate on the board.',
+    experience: 'This project gave me an opportunity to practice my Object Oriented Programming skills by making classes and modules to support each distinct piece without rewriting any code. I also got to write algorithms to detect if there was a check or checkmate on the board. To test my algorithms I used Rspec.',
     siteUrl: "https://replit.com/@chrismenke45/Chess?v=1",
     githubUrl: "https://github.com/chrismenke45/chess",
     desktopPhoto: "./ProjectPhotos/chess/desktop.png",
