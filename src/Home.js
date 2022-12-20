fec8dcfd9e1af9174ce8beb164f6472a9ca3db88
chrismenke45 <chrismenke45@gmail.com>
2022-12-20 15:23:56 -0800

    removed unecessary wording

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <div id="home">
             <h1>Hi, I'm Chris!</h1>
             <img id='photo' className="fadeIn" src='./IMG_4657.jpg' alt='Me'></img>
-            <p>I'm a web developer with experience building websites with HTML, CSS, JavaScript, React, Node.js and MongoDB. I have also started to learn Ruby on Rails and SQL. I’m looking to gain more professional experience. Please checkout my projects and <a href="Resume.pdf" download={true}>resume</a>.  Reach out to me if you think I’d be the right fit for you!</p>
+            <p>I'm a web developer with experience building websites with HTML, CSS, JavaScript, React, Node.js and MongoDB. I have also started to learn Ruby on Rails and SQL. Please checkout my projects and <a href="Resume.pdf" download={true}>resume</a>.  Reach out to me if you think I’d be the right fit for you!</p>
         </div>
     );
 }
