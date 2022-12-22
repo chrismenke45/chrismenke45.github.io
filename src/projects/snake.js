2daced691855314757476890353579b2e1295ba7
chrismenke45 <chrismenke45@gmail.com>
2022-12-22 13:50:14 -0800

    typo fix

--- src/projects/snake.js
+++ src/projects/snake.js
@@ -1,7 +1,7 @@
 const snake = {
     name: "Snake Game",
     description: "This is project is a full stack site using React.js for the front end and Ruby on Rails for an api to store high scores.",
-    experience: 'I made this because I wanted practice building a simple rails API and wanted to write some JavaScript/React after spending the past two months focusing on Ruby and Ruby on Rails. It allowed me to reorient myself with JavaScript while applying some bet practices I learned while studying Ruby. I also used Jest to test some funtionality of the snake game before creating the layout in React.',
+    experience: 'I made this because I wanted practice building a simple rails API and wanted to write some JavaScript/React after spending the past two months focusing on Ruby and Ruby on Rails. It allowed me to reorient myself with JavaScript while applying some best practices I learned while studying Ruby. I also used Jest to test some funtionality of the snake game before creating the layout in React.',
     siteUrl: "https://chrismenke45.github.io/snake_client/",
     githubUrl: "https://github.com/chrismenke45/snake_client",
     desktopPhoto: "./ProjectPhotos/snake/desktop.png",
