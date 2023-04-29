34394e727798d93dd8b760732802a6e06f7598e9
chrismenke45 <chrismenke45@gmail.com>
2023-04-28 18:47:57 -0700

    added incomplete trivia information

---
+++ src/projects/trivia.js
@@ -0,0 +1,12 @@
+const trivia = {
+    name: "Trivia",
+    description: '',
+    experience: '',
+    siteUrl: "https://chrismenke45.github.io/trivia",
+    githubUrl: "https://github.com/chrismenke45/trivia",
+    desktopPhoto: "./ProjectPhotos/trivia/desktop.png",
+    mobilePhoto: "./ProjectPhotos/trivia/mobile.png",
+    id: 15,
+
+}
+export default twitterClone
