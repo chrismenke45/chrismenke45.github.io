f4c91d2a6b5a3a4395b13e11c3dbdaab69dac396
chrismenke45 <chrismenke45@gmail.com>
2023-05-06 10:01:42 -0700

    updated trivia info and added to project list

--- src/Projects.js
+++ src/Projects.js
@@ -3,12 +3,13 @@ import UnderLineStyle from "./UnderLineStyle"
 import IndividualProject from "./IndividualProject";
 import instaClone from "./projects/instaClone";
 import twitterClone from "./projects/twitterClone";
+import trivia from "./projects/trivia"
 import chess from "./projects/chess";
 import snake from "./projects/snake";
 
 
 function Projects() {
-    const projects = [instaClone, twitterClone, snake, chess]
+    const projects = [instaClone, trivia, twitterClone, snake, chess]
     return (
         <section id="projects">
                 <h2 className="sectionTitle">My Projects:</h2>
--- src/projects/trivia.js
+++ src/projects/trivia.js
@@ -1,7 +1,7 @@
 const trivia = {
     name: "Trivia",
-    description: '',
-    experience: 'Tailwind CSS',
+    description: 'This site asks users trivia questions that have been fetched from an external API. It was built with Vanilla TypeScript, Tailwind CSS and utilized Webpack as a module bundler.',
+    experience: 'I built this site to practice Tailwind CSS. It gave me a great introduction to rapidly styling sites with the tool.',
     siteUrl: "https://chrismenke45.github.io/trivia",
     githubUrl: "https://github.com/chrismenke45/trivia",
     desktopPhoto: "./ProjectPhotos/trivia/desktop.png",
@@ -9,4 +9,4 @@ const trivia = {
     id: 15,
 
 }
-export default twitterClone
+export default trivia
