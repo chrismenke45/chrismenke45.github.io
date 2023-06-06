f58f86e8ffd3e9c0c35ce171be5a482ac607f7f0
chrismenke45 <chrismenke45@gmail.com>
2023-06-05 18:20:00 -0700

    edit to say I already learned python

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About() {
         <section id="about">
             <p>
                 I'm a web developer with experience building websites with HTML, CSS, JavaScript, React, Node.js, Ruby on Rails, SQL and MongoDB. 
-                I have also started to learn Python/Flask for my internship. Please checkout my projects and <a href="Resume.pdf" download={true}>resume</a>.  
+                I recently learned Python/Flask for my internship. Please checkout my projects and <a href="Resume.pdf" download={true}>resume</a>.  
                 Reach out to me if you think I’d be the right fit for you!
             </p>
             {/*<img id='photo' class="slowFadeIn" src='./IMG_4657.jpg' alt='Me'></img>*/}
